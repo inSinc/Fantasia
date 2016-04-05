0c551bfb7edc12188a1f48a0a3c9595b9e323122
Sinclair Toffa <sinclair210@gmail.com>
2016-04-05 00:00:41 -0400

    Initial Commit

---
+++ FantasiaTests/FantasiaTests.swift
@@ -0,0 +1,36 @@
+//
+//  FantasiaTests.swift
+//  FantasiaTests
+//
+//  Created by Sinclair on 4/5/16.
+//  Copyright © 2016 Sinclair. All rights reserved.
+//
+
+import XCTest
+@testable import Fantasia
+
+class FantasiaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
