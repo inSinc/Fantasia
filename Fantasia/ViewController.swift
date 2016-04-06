2d237dc1a818de9403ca953062da15aecdde7a75
Sinclair Toffa <sinclair210@gmail.com>
2016-04-05 23:15:14 -0400

    NSTIMER TIME

--- Fantasia/ViewController.swift
+++ Fantasia/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     var imageStrings = [String]()
     var audioStrings = [String]()
     var timer = NSTimer()
+    var stimuliTimer = NSTimer()
+    var ratingTimer = NSTimer()
     var current = 0
     
     @IBOutlet weak var currentImage: UIImageView!
@@ -38,10 +40,12 @@ class ViewController: UIViewController {
         sadFace.hidden = true
         ratingSlider.hidden = true
         ratingSlider.value = 0.0
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("run"), userInfo: nil, repeats: false)
+        run()
     }
+    
     @IBAction func beginButton(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("run"), userInfo: nil, repeats: false)
+        //run()
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: Selector("run"), userInfo: nil, repeats: false)
         beginButton.hidden = true
     }
     
@@ -49,40 +53,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blackColor()
         loadData()
-        currentImage.hidden = true
         ratingSlider.hidden = true
         happyFace.hidden = true
         sadFace.hidden = true
     }
     
     func run(){
-        var currentTime:Double = CACurrentMediaTime()
+        //let currentTime:Double = CACurrentMediaTime()
+        currentImage.hidden = false
+        currentImage.image = visualStimuli[current].image
         auditoryStimuli[current].player.play()
-        //play auditory stimulus for 8 seconds
-        while((CACurrentMediaTime()-currentTime) < 5.0){}
-        auditoryStimuli[current].player.stop()
-        
-        currentImage.image = UIImage(named: "image0.jpeg")
-        
-        //add image layer
-        let imageView:UIImageView = UIImageView(image: UIImage(named: "image0.jpeg"))
-        let viewBoundaries = self.view.bounds
-        let viewHeight = viewBoundaries.height
-        let viewWidth = viewBoundaries.width
-        imageView.bounds = CGRectMake(viewWidth/2, viewHeight/2, 200.0, 200.0)
-        imageView.contentMode = UIViewContentMode.ScaleAspectFit
-        imageView.alpha = 1.0
-        print(imageView)
-        self.view.addSubview(imageView)
-        self.view.bringSubviewToFront(imageView)
-        print(self.view)
-        currentTime = CACurrentMediaTime()
-        //show visual stimuli for 1 second
-        while((CACurrentMediaTime()-currentTime) < 1.0){}
-        imageView.removeFromSuperview()
+        stimuliTimer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("stopStimuli"), userInfo: nil, repeats: false)
+        ratingTimer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("showRating"), userInfo: nil, repeats: false)
+    }
+    
+    func showRating(){
         happyFace.hidden = false
         sadFace.hidden = false
         ratingSlider.hidden = false
+    }
+    
+    func stopStimuli(){
+        currentImage.hidden = true
+        auditoryStimuli[current].player.stop()
     }
     
     func loadData(){
