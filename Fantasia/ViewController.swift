c1e3575608e1bafe626fddaa3639d7c3eaf50373
Sinclair Toffa <sinclair210@gmail.com>
2016-04-05 22:53:50 -0400

    uiimage

--- Fantasia/ViewController.swift
+++ Fantasia/ViewController.swift
@@ -21,8 +21,9 @@ class ViewController: UIViewController {
     var imageStrings = [String]()
     var audioStrings = [String]()
     var timer = NSTimer()
-    var current = 0;
+    var current = 0
     
+    @IBOutlet weak var currentImage: UIImageView!
     @IBOutlet weak var sadFace: UIImageView!
     @IBOutlet weak var happyFace: UIImageView!
 
@@ -33,32 +34,35 @@ class ViewController: UIViewController {
         visualStimuli[current].userImageRating = ratingSlider.value
         print(ratingSlider.value)
         current++
-        run()
+        happyFace.hidden = true
+        sadFace.hidden = true
+        ratingSlider.hidden = true
+        ratingSlider.value = 0.0
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("run"), userInfo: nil, repeats: false)
     }
     @IBAction func beginButton(sender: AnyObject) {
-        //timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("run"), userInfo: nil, repeats: true)
-        run()
-        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("run"), userInfo: nil, repeats: false)
+        beginButton.hidden = true
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blackColor()
         loadData()
+        currentImage.hidden = true
         ratingSlider.hidden = true
         happyFace.hidden = true
         sadFace.hidden = true
     }
     
     func run(){
-        beginButton.hidden = true
-        happyFace.hidden = true
-        sadFace.hidden = true
-        ratingSlider.hidden = true
         var currentTime:Double = CACurrentMediaTime()
-        //auditoryStimuli[current].player.play()
+        auditoryStimuli[current].player.play()
         //play auditory stimulus for 8 seconds
-        //while((CACurrentMediaTime()-currentTime) < 8.0){}
-        //auditoryStimuli[current].player.stop()
+        while((CACurrentMediaTime()-currentTime) < 5.0){}
+        auditoryStimuli[current].player.stop()
+        
+        currentImage.image = UIImage(named: "image0.jpeg")
         
         //add image layer
         let imageView:UIImageView = UIImageView(image: UIImage(named: "image0.jpeg"))
@@ -74,7 +78,7 @@ class ViewController: UIViewController {
         print(self.view)
         currentTime = CACurrentMediaTime()
         //show visual stimuli for 1 second
-        while((CACurrentMediaTime()-currentTime) < 5.0){}
+        while((CACurrentMediaTime()-currentTime) < 1.0){}
         imageView.removeFromSuperview()
         happyFace.hidden = false
         sadFace.hidden = false
@@ -86,10 +90,13 @@ class ViewController: UIViewController {
             imageStrings.append("image\(i)")
             audioStrings.append("audio\(i)")
         }
-        visualStimuli.append(VisualStimulus(imageName: imageStrings[0], imageRating: 5))
-        visualStimuli.append(VisualStimulus(imageName: imageStrings[1], imageRating: -4))
-        auditoryStimuli.append(AuditoryStimulus(audioTrackName: audioStrings[0], audioTrackRating: -3))
-        auditoryStimuli.append(AuditoryStimulus(audioTrackName: audioStrings[1], audioTrackRating: 2))
+        for i in 0...10{
+            visualStimuli.append(VisualStimulus(imageName: imageStrings[0], imageRating: 5))
+            visualStimuli.append(VisualStimulus(imageName: imageStrings[1], imageRating: -4))
+            auditoryStimuli.append(AuditoryStimulus(audioTrackName: audioStrings[0], audioTrackRating: -3))
+            auditoryStimuli.append(AuditoryStimulus(audioTrackName: audioStrings[1], audioTrackRating: 2))
+        }
+        
     }
     
     func pushToFirebase(){
